12b3e387232f2d87b90ea7da077830a185012ee8
Aylmoa <vatilloloco@gmail.com>
2021-12-05 10:59:28 -0600

    Se cambia como lee los archivos para que sean uno por cada clase, al igual que se cambia el metodo de guardar citas

--- src/main/java/Cita.java
+++ src/main/java/Cita.java
@@ -1,23 +1,64 @@
 
-import java.time.*;
-import java.util.Date;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class Cita {
-    int idCita;
-    String fecha;
-    String motivo;
-    String doctor;
-    String paciente;
+    private int idCita;
+    private String fecha;
+    private String motivo;
+    private String doctor;
+    private String paciente;
     private static final AtomicInteger count = new AtomicInteger(0);
     Cita(){
     }
     Cita( String fecha, String motivo, String doc, String paciente){
-        idCita=count.incrementAndGet();
-        this.fecha=fecha;
-        this.motivo=motivo;
-        doctor=doc;
-        this.paciente=paciente;
+        setIdCita(getCount().incrementAndGet());
+        this.setFecha(fecha);
+        this.setMotivo(motivo);
+        setDoctor(doc);
+        this.setPaciente(paciente);
     }
 
+    public static AtomicInteger getCount() {
+        return count;
+    }
+
+    public String getDoctor() {
+        return doctor;
+    }
+
+    public void setDoctor(String doctor) {
+        this.doctor = doctor;
+    }
+
+    public int getIdCita() {
+        return idCita;
+    }
+
+    public void setIdCita(int idCita) {
+        this.idCita = idCita;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getMotivo() {
+        return motivo;
+    }
+
+    public void setMotivo(String motivo) {
+        this.motivo = motivo;
+    }
+
+    public String getPaciente() {
+        return paciente;
+    }
+
+    public void setPaciente(String paciente) {
+        this.paciente = paciente;
+    }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -28,7 +28,7 @@ public class Main {
     public static void menu() throws IOException {
         Scanner sc= new Scanner(System.in);
         String cont="o";String nombre;
-        load();
+        //load();
 
         do{
             System.out.println("Porfavor escoga que es lo que va a querer hacer\n1= Registrar Doctor 2= Registrar Paciente" +
@@ -40,9 +40,6 @@ public class Main {
                         System.out.println("Cual es el nombre del Doctor?"); nombre= sc.next();
                         System.out.println("Cual es la especialidad del Doctor?");String espe= sc.next();
                         crearDoc(nombre,espe);
-                        //Doctor doc= new Doctor(nombre,espe);
-                        //String doce=mapper.writeValueAsString(doc);
-                        //System.out.println(doce);
                         break;
                     case 2:
                         System.out.println("Cual es el nombre del paciente nuevo(a)?");nombre= sc.next();
@@ -59,65 +56,60 @@ public class Main {
                         }
                         break;
                     case 0:
-                        Save();
+                        //Save("citas");
                         cont="n";
                         break;
                     default:
                         System.out.println("Opción Invalida, vuelva a escoger");
                         break;
-                }}catch(NumberFormatException | JsonProcessingException exception){
-                System.out.println("----No escribiste un numero intenta de nuevo---");} catch (IOException e) {
+                }}catch(NumberFormatException ete){
+                System.out.println("----No escribiste un numero intenta de nuevo---");
+            } catch (IOException e) {
                 e.printStackTrace();
             }
         }while(cont.equals("o"));
     }
         public static void crearDoc(String nombre, String especialidad) throws IOException {
-            Doctor doc= new Doctor(nombre,especialidad);
+        total.clear();load("doctores");
+        Doctor doc= new Doctor(nombre,especialidad);
             String doce= mapper.writeValueAsString(doc);total.add(doce);
-           // Save();
+            Save("doctores");
         }
     public static void crearPaciente(String nombre) throws IOException {
+        total.clear();load("pacientes");
         Paciente paciente= new Paciente(nombre);
         String pac= mapper.writeValueAsString(paciente);total.add(pac);
-       // Save();
+        Save("pacientes");
     }
     public static void crearCita(String fecha, String razon,String Doc,String pac) throws IOException {
+        total.clear();load("citas");
         Cita cit= new Cita(fecha,razon,Doc,pac);
         String cita= mapper.writeValueAsString(cit);total.add(cita);
-        //Save();
+        Save("citas");
     }
-        public static void load(String json){
-        Gson gson = new Gson();
-        Cita cita= gson.fromJson(json,Cita.class);
 
-        }
     public static boolean Verificar(String doc, String paciente){
         boolean aux= false;
         boolean aux2=false;
-        try (BufferedReader reader = Files.newBufferedReader(origi, Charset.defaultCharset()); ) {
+        try (BufferedReader reader = Files.newBufferedReader(camino("pacientes"), Charset.defaultCharset());
+             BufferedReader reader2 = Files.newBufferedReader(camino("doctores"), Charset.defaultCharset()); ) {
             //mapper.reader();
 
             Gson gson = new Gson();
+
                 String line=null;
                 while ((line = reader.readLine()) != null){
-                    if(line.contains("fecha")){}
-                    else if(line.contains("especialidad")){
-                        ///String[] doce=line.split("especialidad:");
-                        //Doctor doct= gson.fromJson(line,Doctor.class);
-                        if(line.contains(doc))aux=true;
                         if(line.contains(paciente))aux2=true;
-                    }
-
             }
+                while ((line= reader2.readLine())!=null){
+                    if(line.contains(doc))aux=true;
+                }
         } catch (IOException x) {System.err.format("IOException: %s%n", x);}
-        if(aux==true && aux2==true)return true;
-        else return false;
+        return aux && aux2;
     }
-    public static void load() throws IOException {
+    public static void load(String des) throws IOException {
         ArrayList<String> ola= new ArrayList<>();
-        String separator = FileSystems.getDefault().getSeparator();
-        String filename = String.format("src%smain%sresources%scitas", separator, separator, separator);
-        Path orig = Paths.get(filename);origi=orig;
+       Path orig=camino(des);
         try (BufferedReader reader = Files.newBufferedReader(orig, Charset.defaultCharset()); ) {
             String line=null;
             while (( line=reader.readLine()) != null) {
@@ -128,19 +120,23 @@ public class Main {
         }
     }
 
-    public static void Save( ) throws IOException {
-        String separator = FileSystems.getDefault().getSeparator();
-        String filename = String.format("src%smain%sresources%scitas", separator, separator,separator);
-        Path orig = Paths.get(filename);origi=orig;
+    public static void Save( String destino) throws IOException {
+        Path orig=camino(destino);
         try (BufferedWriter writer = Files.newBufferedWriter(orig, Charset.defaultCharset())){
+
             for (Object ete:total)
-            {mapper.configure(DeserializationFeature.USE_JAVA_ARRAY_FOR_JSON_ARRAY, true);
+            {
                 writer.write(String.valueOf(ete));
             }
 
         } catch (IOException x) {
             System.err.format("IOException: %s%n", x);}
     }
-
+    public static Path camino(String destino){
+        String separator = FileSystems.getDefault().getSeparator();
+        String filename = String.format("src%smain%sresources%s%s", separator, separator, separator,destino);
+        Path orig = Paths.get(filename);
+        return orig;
+    }
 
 }
