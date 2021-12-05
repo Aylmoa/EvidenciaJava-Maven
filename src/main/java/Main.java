45de07d2e5f093743f256a5540e4edaa782f613d
Aylmoa <vatilloloco@gmail.com>
2021-12-05 10:01:40 -0600

    Se agrega el sistema de login con unos usuarios pre cargados

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -18,6 +18,7 @@ import java.util.Scanner;
 
 public class Main {
     static ArrayList<Object>total= new ArrayList<Object>();
+    static ArrayList<Usuario>Usuarios= new ArrayList();
     static Path origi=null;
     static ObjectMapper mapper= new ObjectMapper();
 
@@ -29,48 +30,64 @@ public class Main {
         Scanner sc= new Scanner(System.in);
         String cont="o";String nombre;
         load();
+        cargarUsu();
+        System.out.println("Username:");String usr= sc.nextLine();
+        System.out.println("Password:");String pass= sc.nextLine();
+        Boolean existe; existe=validarUsuarios(usr,pass);
+        if(existe) {
+            System.out.println("Bienvenido");
+            do {
+                System.out.println("Porfavor escoga que es lo que va a querer hacer\n1= Registrar Doctor 2= Registrar Paciente" +
+                        "3= Registrar nueva cita 0= Salir");
+                try {
+                    int eleccion = Integer.parseInt(sc.next());
+                    switch (eleccion) {
+                        case 1:
+                            System.out.println("Cual es el nombre del Doctor?");
+                            nombre = sc.next();
+                            System.out.println("Cual es la especialidad del Doctor?");
+                            String espe = sc.next();
+                            crearDoc(nombre, espe);
+                            //Doctor doc= new Doctor(nombre,espe);
+                            //String doce=mapper.writeValueAsString(doc);
+                            //System.out.println(doce);
+                            break;
+                        case 2:
+                            System.out.println("Cual es el nombre del paciente nuevo(a)?");
+                            nombre = sc.next();
+                            crearPaciente(nombre);
+                            break;
+                        case 3:
+                            System.out.println("Cuál es la fecha y hora de la cita?");
+                            String fecha = sc.next();
+                            System.out.println("Cuál es la razón de la cita?");
+                            String razon = sc.next();
+                            System.out.println("Que doctor antenderá?");
+                            String doc = sc.next();
+                            System.out.println("Que paciente atenderá?");
+                            String pac = sc.next();
+                            if (Verificar(doc, pac)) crearCita(fecha, razon, doc, pac);
+                            else {
+                                System.out.println("No se encuentran el doctor o el paciente en la base de datos");
+                            }
+                            break;
+                        case 0:
+                            Save();
+                            cont = "n";
+                            break;
+                        default:
+                            System.out.println("Opción Invalida, vuelva a escoger");
+                            break;
+                    }
+                } catch (NumberFormatException | JsonProcessingException exception) {
+                    System.out.println("----No escribiste un numero intenta de nuevo---");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            } while (cont.equals("o"));
+        }else System.out.println("No existe el Usario o contraseña, intente de nuevo");
 
-        do{
-            System.out.println("Porfavor escoga que es lo que va a querer hacer\n1= Registrar Doctor 2= Registrar Paciente" +
-                    "3= Registrar nueva cita 0= Salir");
-            try{
-                int eleccion= Integer.parseInt(sc.next());
-                switch (eleccion){
-                    case 1:
-                        System.out.println("Cual es el nombre del Doctor?"); nombre= sc.next();
-                        System.out.println("Cual es la especialidad del Doctor?");String espe= sc.next();
-                        crearDoc(nombre,espe);
-                        //Doctor doc= new Doctor(nombre,espe);
-                        //String doce=mapper.writeValueAsString(doc);
-                        //System.out.println(doce);
-                        break;
-                    case 2:
-                        System.out.println("Cual es el nombre del paciente nuevo(a)?");nombre= sc.next();
-                        crearPaciente(nombre);
-                        break;
-                    case 3:
-                        System.out.println("Cuál es la fecha y hora de la cita?");String fecha= sc.next();
-                        System.out.println("Cuál es la razón de la cita?");String razon= sc.next();
-                        System.out.println("Que doctor antenderá?");String doc= sc.next();
-                        System.out.println("Que paciente atenderá?");String pac= sc.next();
-                        if (Verificar(doc, pac))crearCita(fecha,razon,doc,pac);
-                        else{
-                            System.out.println("No se encuentran el doctor o el paciente en la base de datos");
-                        }
-                        break;
-                    case 0:
-                        Save();
-                        cont="n";
-                        break;
-                    default:
-                        System.out.println("Opción Invalida, vuelva a escoger");
-                        break;
-                }}catch(NumberFormatException | JsonProcessingException exception){
-                System.out.println("----No escribiste un numero intenta de nuevo---");} catch (IOException e) {
-                e.printStackTrace();
-            }
-        }while(cont.equals("o"));
-    }
+        }
         public static void crearDoc(String nombre, String especialidad) throws IOException {
             Doctor doc= new Doctor(nombre,especialidad);
             String doce= mapper.writeValueAsString(doc);total.add(doce);
@@ -86,6 +103,19 @@ public class Main {
         String cita= mapper.writeValueAsString(cit);total.add(cita);
         //Save();
     }
+    public static void cargarUsu(){
+        if (Usuarios==null){
+            Usuarios= new ArrayList<>();
+        }
+
+        Usuarios.add(new Usuario("Admin","123"));
+        Usuarios.add(new Usuario("Leonardz","Comida"));
+        System.out.println("Exito");
+    }
+    public static boolean validarUsuarios(String Usr, String Pass){
+           return Usuarios.stream().anyMatch(s -> s.getPassword().equals(Pass) && s.getUsername().equals(Usr));
+    }
+
         public static void load(String json){
         Gson gson = new Gson();
         Cita cita= gson.fromJson(json,Cita.class);
@@ -95,15 +125,12 @@ public class Main {
         boolean aux= false;
         boolean aux2=false;
         try (BufferedReader reader = Files.newBufferedReader(origi, Charset.defaultCharset()); ) {
-            //mapper.reader();
 
             Gson gson = new Gson();
                 String line=null;
                 while ((line = reader.readLine()) != null){
                     if(line.contains("fecha")){}
                     else if(line.contains("especialidad")){
-                        ///String[] doce=line.split("especialidad:");
-                        //Doctor doct= gson.fromJson(line,Doctor.class);
                         if(line.contains(doc))aux=true;
                         if(line.contains(paciente))aux2=true;
                     }
